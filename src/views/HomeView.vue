3eb82061a8e020ba22dc2debb3e3c66693cd317b
dani martin <danimartinjuarez@gmail.com>
2022-06-29 21:36:48 +0200

    Pruebas de api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,23 @@
 <script setup>
 import SearchBar from '../components/my-components/searchBar.vue';
 import PanelIndex from '../components/my-components/panelIndex.vue';
+import { dataAPI } from '../stores/dataAPI.js';  
+import { computed } from 'vue';
+console.log(dataAPI);
 </script>
 
 <template>
   <main>
     <SearchBar></SearchBar>
     <PanelIndex></PanelIndex>
+    <div><p>{{ dataAPI }}</p></div>
   </main>
 </template>
 
 <style lang="css" scoped>
     main{
       height: 70vh;
+      background-color: ;
     }
     
 </style>
