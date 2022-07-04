305bdb117c43f22877bd2a4b63d178b7be9385bf
sniper <mensitaoufik29@gmail.com>
2022-07-04 15:40:13 +0200

    creada tarjeta de favoritos

---
+++ src/components/my-components/card.vue
@@ -0,0 +1,74 @@
+<script>
+    export default{
+
+    }
+</script>
+<template>
+    <div class="cardBox">
+        <div id="photo">
+             <img src="https://static.wikia.nocookie.net/disney/images/f/fa/Normal_EoA_S3_E4_0217.jpg" id="characterImg"/>
+             <div class="stars">
+                <img src="\src\images\star-grey.png" id="star1"/>
+                <img src="\src\images\star-grey.png" id="star2"/>
+                <img src="\src\images\star-grey.png" id="star3"/>
+             </div>
+        </div>
+         <div class="cardtext">
+            <h5 id="name">Name: </h5> 
+            <p id="nameData"></p>
+            <h5 id="film">Film or Show: </h5>
+            <p id="filmData"></p>
+            <h5 id="allies">Allies: </h5>
+            <p id="alliesData"></p>
+            <h5 id="enemies">Enemies: </h5>
+            <p id="enemiesData"></p>
+        </div>
+        <div class="cardButtons">
+            <button id="delete">
+                <img src="/src/images/delete.png" id="deleteImg"/>
+            </button>
+            <button id="edit">
+                <img src="/src/images/edit.png" id="editImg"/>
+            </button>
+        </div>
+    </div>
+    
+</template>
+
+<style>
+.cardBox{
+    background-color:#39C1CD;
+    display: flex;
+    justify-content: center;
+}
+.cardButtons{
+    display: flex;
+    flex-direction:column;
+}
+.cardtext{
+    width:35vw;
+    height: 18vh;
+    background-color: wheat;
+    line-height: 1px;
+    
+}
+#photo{
+    width: 8vw;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+#deleteImg, #editImg{
+    width: 2vw;
+
+}
+
+.stars img{
+    width:1vw;
+    
+}
+#characterImg{
+    height: 15vh;
+}
+
+</style>
--- src/components/my-components/panelFav.vue
+++ src/components/my-components/panelFav.vue
@@ -1,13 +1,14 @@
 
 <script>
+import Card from './card.vue';
 export default {
-    
+    components: { Card }
 }
 </script>
 
 <template>
     <div class="panelFav">
-        
+       <Card></Card> 
     </div>
 </template>
 
